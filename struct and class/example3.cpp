f09c651bd5feeb6951c1ffc7fc972a4924319d62
ShimejiAnna4191 <ilikecake4191@gmail.com>
2024-06-20 21:32:21 +0700

    add class example

---
+++ struct and class/example3.cpp
@@ -0,0 +1,30 @@
+//class example
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class Person
+{
+private:
+    std::string m_name{};
+public:
+    void hug(const Person& h)const
+    {
+        std::cout <<m_name<<" hug "<<h.m_name<<'\n';
+    }
+    void setPerson(std::string_view name)
+    {
+        m_name = name;
+    }
+};
+
+int main(){
+    Person Anna;
+    Anna.setPerson("Anna");
+
+    Person Ivan;
+    Ivan.setPerson("Ivan");
+
+    Anna.hug(Ivan);
+    return 0;
+}
