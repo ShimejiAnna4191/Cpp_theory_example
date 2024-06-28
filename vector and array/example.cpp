9722e106dd48a92f3522b9ed7de4545c2d49b293
ShimejiAnna4191 <ilikecake4191@gmail.com>
2024-06-28 20:31:15 +0700

    vector and array

---
+++ vector and array/example.cpp
@@ -0,0 +1,14 @@
+//From chapter 15 onwards, it seemed like I started to get acquainted with the native C++ library instead of keys, structures, and functions
+//Most of these libraries are important because they are standard libraries
+//they will contribute as key work of c++
+
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> arr {2, 4, 5};  
+
+    std::cout<<arr[1];
+    return 0; 
+}
+
