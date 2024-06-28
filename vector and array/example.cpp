8a9e6c4f338fc457bed843768286b0a4554170ab
ShimejiAnna4191 <ilikecake4191@gmail.com>
2024-06-28 20:38:43 +0700

    Update example.cpp

--- vector and array/example.cpp
+++ vector and array/example.cpp
@@ -1,7 +1,7 @@
 //From chapter 15 onwards, it seemed like I started to get acquainted with the native C++ library instead of keys, structures, and functions
 //Most of these libraries are important because they are standard libraries
 //they will contribute as key work of c++
-//vector and arr will be the first point
+//vector and arr will be the first stop
 
 #include <iostream>
 #include <vector>
