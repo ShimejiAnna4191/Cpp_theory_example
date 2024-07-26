336edd3bc442ba17d16fe7beb89df93633ff8ae5
ShimejiAnna4191 <ilikecake4191@gmail.com>
2024-07-26 21:33:32 +0700

    The Function pointed method

---
+++ Cpp-Advanced/The function pointers/example2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void print(int x)
+{
+    std::cout << "print(int)\n";
+}
+
+void print(int x, int y = 10)
+{
+    std::cout << "print(int, int)\n";
+}
+
+int main()
+{
+    // Deconstructed method
+    using vnptr = void(*)(int); // define a type alias for a function pointer to a void(int) function
+    vnptr pi { print }; // initialize our function pointer with function print
+    pi(1); // call the print(int) function through the function pointer
+
+    // Concise method
+    static_cast<void(*)(int)>(print)(1); // call void(int) version of print with argument 1
+
+    return 0;
+}
