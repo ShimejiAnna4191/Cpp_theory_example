0a6738d97842b1c773c2233bffe1f26b9a60497e
ShimejiAnna4191 <ilikecake4191@gmail.com>
2024-08-05 22:02:44 +0700

    Lambda capt you

---
+++ Cpp-Advanced/Lambda captures/example.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <string_view>
+
+int main()
+{
+  std::array<std::string_view, 4> arr{ "hutao", "walnut", "yoimiya","shimejianna" };
+
+  auto found{ std::find_if(arr.begin(), arr.end(),
+                           [](std::string_view str)
+                           {
+                             return str.find("nut") != std::string_view::npos;
+                           }) };
+
+  if (found == arr.end())
+  {
+    std::cout << "No nuts\n";
+  }
+  else
+  {
+    std::cout << "Found " << *found << '\n';
+  }
+
+  return 0;
+}
