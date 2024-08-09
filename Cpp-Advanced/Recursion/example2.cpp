e49228ddc0e24b8c7526f2204f54d15583de4de1
ShimejiAnna4191 <ilikecake4191@gmail.com>
2024-08-09 22:55:24 +0700

    more example about recursion

---
+++ Cpp-Advanced/Recursion/example2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void printBinary(unsigned int n)
+{
+	if (n > 1) 
+	{
+		printBinary(n / 2);
+	}
+	std::cout << n % 2;
+}
+
+int main()
+{
+	int x{};
+	std::cout << "Enter an integer: ";
+	std::cin >> x;
+
+	printBinary(static_cast<unsigned int>(x));
+}
