c54b6957ce261f58597bcabd94cde82167f0ecb1
ShimejiAnna4191 <ilikecake4191@gmail.com>
2024-08-15 21:33:52 +0700

    Delete Vietnamese comment

--- C-style arrays/example3.cpp
+++ C-style arrays/example3.cpp
@@ -5,20 +5,17 @@ int main(){
     char chr[] {"hello"};
     const char name[] {"Yoimiya"};
 
-    //tất cả mảng dưới đây bị phân rã thành kiểu con trỏ
-    std::cout<<number<<"\n"; //kì lạ thay mảng int in dưới dang địa chỉ trong khi mảng char in dưới dạng chuỗi
+    std::cout<<number<<"\n"; 
     std::cout<<chr<<"\n";
     std::cout<<name<<"\n";
     
     std::cout<<static_cast<const void*>(name)<<"\n"; 
-    // không thể in con trỏ char dưới dạng địa chỉ bằng cách thông thường (&name), ta sẽ in bằng cách `static_cast<const void*>(name)`
     return 0;
 }
 
 // int main()
 // {
 //     char c{ 'Q' };
-//     std::cout << &c; //hành vi không xác định do kiểu chả không thể bị chấm dứt
-
+//     std::cout << &c; 
 //     return 0;
 // }
