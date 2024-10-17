532093983ad28895a19b6bdaad172db8dfba0db7
ShimejiAnna4191 <ilikecake4191@gmail.com>
2024-10-17 23:22:48 +0700

    Pointers and references to the base class of derived objects

---
+++ Virtual Functions/example1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string_view>
+
+class Base
+{
+protected:
+    int m_value {};
+
+public:
+    Base(int value)
+        : m_value{ value }
+    {
+    }
+
+    std::string_view getName() const { return "Base"; }
+    int getValue() const { return m_value; }
+};
+
+class Derived: public Base
+{
+public:
+    Derived(int value)
+        : Base{ value }
+    {
+    }
+
+    std::string_view getName() const { return "Derived"; }
+    int getValueDoubled() const { return m_value * 2; }
+};
+
+int main()
+{
+    Derived derived{ 5 };
+    std::cout << "derived is a " << derived.getName() << " and has value " << derived.getValue() << '\n';
+
+    Derived& rDerived{ derived };
+    std::cout << "rDerived is a " << rDerived.getName() << " and has value " << rDerived.getValue() << '\n';
+
+    Derived* pDerived{ &derived };
+    std::cout << "pDerived is a " << pDerived->getName() << " and has value " << pDerived->getValue() << '\n';
+
+    return 0;
+}
